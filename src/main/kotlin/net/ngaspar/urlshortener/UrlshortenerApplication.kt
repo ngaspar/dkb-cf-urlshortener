d1a608a12d7d992fa37f00dfa03140e1d95233dd
ngaspar <gaspaxu@gmail.com>
2023-01-20 15:04:00 +0100

    First commit: Spring Initializer project

---
+++ src/main/kotlin/net/ngaspar/urlshortener/UrlshortenerApplication.kt
@@ -0,0 +1,11 @@
+package net.ngaspar.urlshortener
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlshortenerApplication
+
+fun main(args: Array<String>) {
+	runApplication<UrlshortenerApplication>(*args)
+}
---
+++ src/test/kotlin/net/ngaspar/urlshortener/UrlshortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.ngaspar.urlshortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
